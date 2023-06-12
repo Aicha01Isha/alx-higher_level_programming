c1fbb134f7f55e92a4e9e11e34c9ca7e0bcb42b9
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-06-12 13:08:09 -0700

    py

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: parameter
+ * Return: head
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *nod = *head, *next, *pr = NULL;
+	while (nod)
+	{
+		next = nod->next;
+		node->next = pr;
+		pr = nod;
+		nod = next;
+	}
+	*head = pr;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: parameter
+ * Return: 1 on success, 0 on failure
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *t, *r, *m;
+	size_t size = 0, a;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	t = *head;
+	while (t)
+	{
+		size++;
+		t = t->next;
+	}
+	t = *head;
+	for (a = 0; a < (size / 2) - 1; a++)
+		t = t->next;
+
+	if ((size % 2) == 0 && t->n != t->next->n)
+		return (0);
+	t = t->next->next;
+	r = reverse_listint(&t);
+	m = r;
+	t = *head;
+	while (r)
+	{
+		if (t->n != r->n)
+			return (0);
+		t = t->next;
+		r = r->next;
+	}
+	reverse_listint(&m);
+	return (1);
+}
---
+++ 0x03-python-data_structures/linked_lists.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: parameter
+ * Return: i
+ */
+
+size_t print_listint(const listint_t *h)
+{
+    const listint_t *cur;
+    unsigned int i;
+
+    cur = h;
+    i = 0;
+    while (cur != NULL)
+    {
+	printf("%i\n", cur->i);
+	cur = cur->next;
+	i++;
+    }
+    return (i);
+}
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: parameter
+ * @n: parameter
+ * Return: ne
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+    listint_t *ne;
+    listint_t *cur;
+
+    current = *head;
+    ne = malloc(sizeof(listint_t));
+    if (ne == NULL)
+	return (NULL);
+    ne->n = n;
+    ne->next = NULL;
+    if (*head == NULL)
+	*head = ne;
+    else
+    {
+	while (cur->next != NULL)
+	    cur = cur->next;
+	cur->next = ne;
+    }
+    return (ne);
+}
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: parameter
+ * Return: 0 on success
+ */
+
+void free_listint(listint_t *head)
+{
+    listint_t *cur;
+
+    while (head != NULL)
+    {
+	cur = head;
+	head = head->next;
+	free(cur);
+    }
+}
