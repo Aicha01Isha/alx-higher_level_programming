41a60763f12ab20ae1ebef94d040829d459787d4
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-06-26 15:06:02 -0700

    a

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+"""print"""
+    ret = 0
+    for a in range(x):
+        try:
+            print("{}".format(my_list[a]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
