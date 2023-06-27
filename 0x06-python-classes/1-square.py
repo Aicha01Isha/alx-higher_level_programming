0e1eeedd88c707f5b11690baf402d5e87b19415f
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-06-27 13:53:24 -0700

    a

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define"""
+class Square:
+    """Represent"""
+    def __init__(self, size):
+        """Initialize
+        Args:
+            size (int): The size
+        """
+        self.__size = size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Define"""
+class Square:
+    """Represent"""
+    def __init__(self, size=0):
+        """Initialize
+        Args:
+            size (int): The size
+        """
+        self.size = size
+    @property
+    def size(self):
+        """Get"""
+        return (self.__size)
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def area(self):
+        """Return"""
+        return (self.__size * self.__size)
