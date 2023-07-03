fc3c44c9135376c8c39e9fe064f28512436121c8
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-07-03 14:58:13 -0700

    a

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" empty """
+class Rectangle:
+    """ class """
+    def __init__(self, width=0, height=0):
+        """ Instantiation """
+        self.width = width
+        self.height = height
+    @property
+    def width(self):
+        """ widt"""
+        return self.__width
+    @property
+    def height(self):
+        """heigh"""
+        return self.__height
+    @width.setter
+    def width(self, value):
+        """ width setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    @height.setter
+    def height(self, value):
+        """ height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
